3d6d56cc41ae1f489ab65928d8e734cb5ad37b04
balasdz <121518891+balasdz@users.noreply.github.com>
2024-03-13 20:16:19 +0100

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,80 +1,56 @@
 from flask import Flask, request
-from selenium.webdriver.common.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 
 app = Flask(__name__)
 
 class YouLikeHits:
     def __init__(self):
-        self.setup = Setup()
+        self.browser = None
 
-    def get_point(self):
-        point = self.setup.browser.find_element(By.ID, 'currentpoints')
-        print('Your point is: ' + point.text)
-        return point
+    def init_browser(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    def go_to_website(self):
-        # انتقال إلى موقع YouLikeHits
-        self.setup.browser.get('https://www.youlikehits.com/login.php')
-        WebDriverWait(self.setup.browser, 10).until(EC.presence_of_element_located((By.ID, 'username')))
+    def login(self):
+        self.browser.get('https://www.youlikehits.com/login.php')
+        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, 'username')))
+        username = self.browser.find_element(By.ID, 'username')
+        username.send_keys('am plays')
+        password = self.browser.find_element(By.ID, 'password')
+        password.send_keys('78945612')
+        login_button = self.browser.find_element(By.XPATH, '//input[@name="submit"]')
+        login_button.click()
 
-        # ملء اسم المستخدم وكلمة المرور وتسجيل الدخول
-        uid = self.setup.browser.find_element(By.ID, 'username')
-        uid.send_keys('am plays')
-        pwd = self.setup.browser.find_element(By.ID, 'password')
-        pwd.send_keys('78945612')
-        btn = self.setup.browser.find_element(By.XPATH, '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td/center/form/table/tbody/tr[3]/td/span/input')
-        btn.click()
-
-        print('Getting credits... Please do not terminate the program.')
-        while True:
-            try:
-                self.setup.browser.get('https://www.youlikehits.com/youtubenew2.php')
-                WebDriverWait(self.setup.browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="listall"]/center/a[1]')))
-                yt_view = self.setup.browser.find_element(By.XPATH, '//*[@id="listall"]/center/a[1]')
-                yt_view.click()
-
-                element_present = EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Points Added')]"))
-                WebDriverWait(self.setup.browser, 1000).until(element_present)
-
-            except:
-                while True:
-                    self.setup.browser.get('https://www.youlikehits.com/websites.php')
-                    WebDriverWait(self.setup.browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Visit")))
-                    surf = self.setup.browser.find_element(By.LINK_TEXT, "Visit")
-                    surf.click()
-                    WebDriverWait(self.setup.browser, 10).until(EC.number_of_windows_to_be(2))
-                    self.setup.browser.switch_to.window(self.setup.browser.window_handles[1])
-                    self.setup.browser.close()
-                    self.setup.browser.switch_to.window(self.setup.browser.window_handles[0])
+    def perform_task(self):
+        self.browser.get('https://www.youlikehits.com/youtubenew2.php')
+        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="listall"]/center/a[1]')))
+        youtube_button = self.browser.find_element(By.XPATH, '//*[@id="listall"]/center/a[1]')
+        youtube_button.click()
+        WebDriverWait(self.browser, 1000).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Points Added')]")))
 
     def close_browser(self):
-        self.setup.close_browser()
-
-def integrate_functions():
-    # Call the YouLikeHits function only
-    you_like_hits = YouLikeHits()
-    try:
-        you_like_hits.setup.init()
-        you_like_hits.go_to_website()
-    except:
-        you_like_hits.get_point()
-        print('Content not found to watch or surf. Refreshing webpage...')
-    finally:
-        you_like_hits.close_browser()
+        self.browser.quit()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        integrate_functions()
-        return "Process Completed"
+        ylh = YouLikeHits()
+        ylh.init_browser()
+        ylh.login()
+        ylh.perform_task()
+        ylh.close_browser()
+        return "Task completed successfully!"
 
 if __name__ == '__main__':
-    # Use the Railway-provided port
     port = int(os.getenv("PORT", default=5050))
     app.run(host='0.0.0.0', port=port)
